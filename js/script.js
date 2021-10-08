a92b0c757e513d45878455477f0e6c53c6facb30
jackashi <sadjiabdoulaye@ept.sn>
2021-10-08 10:06:42 +0000

    gérer germe manquant, case 1

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
+//données à envoyer par post
+let antiB_result = [];
+//données du datatable
+let dataSet = [];
+let table;
+$(document).ready(function () {
+    table = $('#dataTable1').DataTable({
+        data: dataSet,
+        columns: [
+            { title: "Germe" },
+            { title: "Antibiotique" },
+            { title: "résultat" } //TODO mettre un flat button pour supprimer
+        ]
+    });
+});
+
 
 //soumission du formulaire
 document.getElementById("sbt_info_mal_bacterio").addEventListener("click", function (e) {
     e.preventDefault();
+    console.log(antiB_result);
     //récupérer tous les champs et les envoyer par ajax : post
 });
 
-//ajouter une nouvelle ligne d'antibiotique au clic
 
-let antiB_result = [];
+
 let addBtn = document.querySelector('#add-new-antibiotique');
 
 addBtn.addEventListener('click', function (e) {
@@ -17,34 +33,64 @@ addBtn.addEventListener('click', function (e) {
     let r2 = document.querySelector('#intermediaire_1');
     let r3 = document.querySelector('#resistant_1');
     let germe1_identifie = document.querySelector('#germe1_identifie').value;
+    let germe1_non_identifie = document.querySelector('#germe1_non_identifie').value;
+
 
     if (antibiotiqueValue === "") {
         alert('entrer la valeur de l\'antibiotique');
     }
-    else if (germe1_identifie == "") {
+    else if (germe1_identifie == "" && germe1_non_identifie == "") {
         alert('germe manquant!');
     } else {
         if (r1.checked == true) {
-            antiB_result.push({ "germe": germe1_identifie, "antibiotique": antibiotiqueValue, "resultat": true })
+            if( germe1_identifie == ""){
+                antiB_result.push({ "germe": germe1_non_identifie, "antibiotique": antibiotiqueValue, "resultat": true });
+                dataSet.push([germe1_non_identifie, antibiotiqueValue, "sensible"]);
+            }
+            else {
+                antiB_result.push({ "germe": germe1_identifie, "antibiotique": antibiotiqueValue, "resultat": true });
+                dataSet.push([germe1_identifie, antibiotiqueValue, "sensible"]);
+            }
+
             r1.checked = false
 
         } else if (r2.checked == true) {
             antiB_result.push({ "germe": germe1_identifie, "antibiotique": antibiotiqueValue, "resultat": true })
             r1.checked = false
+            dataSet.push([germe1_identifie, antibiotiqueValue, "intermédiaire"])
 
 
         } else if (r3.checked == true) {
             antiB_result.push({ "germe": germe1_identifie, "antibiotique": antibiotiqueValue, "resultat": true })
             r1.checked = false
+            dataSet.push([germe1_identifie, antibiotiqueValue, "résistant"])
 
         } else {
             alert('resultat manquant!');
 
         }
         document.querySelector('#antibiotique_1').value = "";
+        document.querySelector('#germe1_identifie').value = "";
+        document.querySelector('#germe1_non_identifie').value = "";
+        r1.checked = false;
+        r2.checked = false;
+        r3.checked = false;
+
+
+
 
         //TODO Mettre à jour la datatable
-        console.log(antiB_result)
+        $('#dataTable1').DataTable().destroy();
+
+        table = $('#dataTable1').DataTable({
+            data: dataSet,
+            columns: [
+                { title: "Germe" },
+                { title: "Antibiotique" },
+                { title: "résultat" } //TODO mettre un flat button pour supprimer
+            ]
+        });
+
 
     }
 });
